c798b360b52d903bbf8e453b1b6d15012648d07d
akshayrajput077 <akshay.devrajput@gmail.com>
2025-09-16 16:32:37 +0530

    new change todolist

--- index.js
+++ index.js
@@ -7,29 +7,38 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 let tasks = [];
-// let tasktodo = '';
 
 app.get('/', (req, res) => {
-  // console.log(tasktodo);
-  res.render('todolist', { taskList: tasks });
+  res.render('todolist', { taskList: tasks, favicon: '/views/favicon.ico' });
 });
 
 app.post('/add', (req, res) => {
   const task = req.body;
   if (task) {
-    tasks.unshift(task);
+    tasks.push(task);
     res.redirect('/');
   }
 });
 
-// app.post('/edit/:index', (req, res) => {
-//   const index = parseInt(req.params.index);
-//   let filterdata = tasks.filter((idx) => idx === index);
-//   if (filterdata[0].task) {
-//     tasktodo.push(filterdata[0].task);
-//     res.redirect('/');
-//   }
-// });
+
+app.post('/edit/:index', (req, res) => {
+  const index = parseInt(req.params.index);
+  if (isNaN(index) || !tasks[index]) {
+    return res.status(404).send('Task not found');
+  }
+  const taskItem = tasks[index];
+  res.render('update', { task: taskItem, index });
+});
+
+app.post('/update/:index', (req, res) => {
+  const index = parseInt(req.params.index);
+  const updatedTask = req.body;
+  if (!isNaN(index) && tasks[index]) {
+    tasks[index] = updatedTask;
+  }
+  res.redirect('/');
+});
+
 
 app.post('/delete/:index', (req, res) => {
   const index = parseInt(req.params.index);
@@ -44,6 +53,6 @@ app.post('/alldelete', (req, res) => {
   res.redirect('/');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
 });
